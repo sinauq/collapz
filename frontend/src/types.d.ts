c8d0168928d8920f4941a8bc18b4b6b78359c869
sinat14 <sinauq@disroot.org>
2026-08-25 22:59:02 +0200

    Add type file

---
+++ frontend/src/types.d.ts
@@ -0,0 +1,27 @@
+import type { DefaultSuggestionItem } from "@blocknote/core";
+
+export type LinkData = {
+  title: string;
+  id: string;
+  relationship: string;
+  content: string;
+};
+
+export type NoteData = {
+  title: string;
+  id: number;
+  relationships: {
+    backLinks: Array<LinkData>;
+    forwardLinks: Array<LinkData>;
+  };
+  content: string;
+  owner: string;
+};
+
+export interface EditorProps {
+  data: NoteData;
+  editorRef?: Ref<MDXEditorMethods> | null;
+  setEditing: (arg0: boolean) => void;
+}
+
+export type NotesSuggestionItem = DefaultSuggestionItem & NoteData;
